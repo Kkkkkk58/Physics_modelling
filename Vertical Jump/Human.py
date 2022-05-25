3df1719b0abb01e72b1cb0c4ede834c3508554c9
Karim Khasan <karimhasan3879@gmail.com>
2022-05-26 01:20:17 +0300

    Optimized Human class

--- Vertical Jump/Human.py
+++ Vertical Jump/Human.py
@@ -8,15 +8,14 @@ class Human:
         self.body_size = body_size / 100.0
         self.squat_depth = squat_depth / 100.0
         self.hip_size = self.height / 4.0
-        self.initial_angle = self.hip_size / (self.hip_size - self.squat_depth)
     
     def evaluate_mcp(self):
         if self.mcp == -1:
 
             if self.sex == "M":
-                self.mcp = (11.066 + 0.675 * self.height * 100 - \
-                    0.173 * self.leg_girth * 100 - 0.299 * self.body_size * 100) / 100.0
+                self.mcp = 0.11066 + 0.675 * self.height - \
+                    0.173 * self.leg_girth - 0.299 * self.body_size
             else:
-                self.mcp = (-4.667 + 0.289 * self.height * 100 + \
-                    0.383 * self.leg_girth * 100 + 0.301 * self.body_size * 100) / 100.0
+                self.mcp = -0.04667 + 0.289 * self.height + \
+                    0.383 * self.leg_girth + 0.301 * self.body_size
         return self.mcp
