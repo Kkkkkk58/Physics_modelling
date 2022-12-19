91ee44d251ac6a833634ff2b71796589be7425cb
Karim Khasan <karimhasan3879@gmail.com>
2022-12-19 03:10:35 +0300

    feat: add textbox

--- Mirage/Ctk_graphics.py
+++ Mirage/Ctk_graphics.py
@@ -3,6 +3,7 @@ import customtkinter
 from tkinter import messagebox
 from values import Values
 from plotter import Plotter
+from PIL import Image
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -31,7 +32,7 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("Mirage Model by A.Gogolev & K.Khasan")
+        self.title("Mirage Model by K.Khasan & A.Gogolev")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
         
@@ -54,27 +55,24 @@ class App(customtkinter.CTk):
         self.frame_left.grid_rowconfigure(13, minsize=75)
         self.entries = []
         self.left_frame_elements = []
-        # Input weight
-        self.label_n0 = customtkinter.CTkLabel(master=self.frame_left, text=App.REQUESTS[Values.INITIAL_REFRACTIVE_INDEX],
-                                                   text_font=("Roboto Medium", -16))
+        # Input initial refracrive index
+        self.label_n0 = customtkinter.CTkLabel(master=self.frame_left, text=App.REQUESTS[Values.INITIAL_REFRACTIVE_INDEX])
         self.label_n0.grid(row=0, column=0, pady=10, padx=10)
         self.entry_n0 = customtkinter.CTkEntry(master=self.frame_left, width=80, placeholder_text="1.0004")
         self.entry_n0.grid(row=1, column=0, columnspan=2, pady=0, padx=10)
         self.entries.append(self.entry_n0)
         self.left_frame_elements.append(self.entry_n0)
 
-        # Input height
-        self.label_angle = customtkinter.CTkLabel(master=self.frame_left, text=App.REQUESTS[Values.ANGLE],
-                                                   text_font=("Roboto Medium", -16))
+        # Input initial angle
+        self.label_angle = customtkinter.CTkLabel(master=self.frame_left, text=App.REQUESTS[Values.ANGLE])
         self.label_angle.grid(row=2, column=0, pady=10, padx=10)
         self.entry_angle = customtkinter.CTkEntry(master=self.frame_left, width=80, placeholder_text="30")
         self.entry_angle.grid(row=3, column=0, columnspan=2, pady=0, padx=10)
         self.entries.append(self.entry_angle)
         self.left_frame_elements.append(self.entry_angle)
 
-        # Input leg girth
-        self.label_gamma = customtkinter.CTkLabel(master=self.frame_left, text=App.REQUESTS[Values.REFRACTIVE_INDEX_COEFFICIENT],
-                                                      text_font=("Roboto Medium", -16))
+        # Input gamma coeffecient 
+        self.label_gamma = customtkinter.CTkLabel(master=self.frame_left, text=App.REQUESTS[Values.REFRACTIVE_INDEX_COEFFICIENT])
         self.label_gamma.grid(row=4, column=0, pady=10, padx=10)
         self.entry_gamma = customtkinter.CTkEntry(master=self.frame_left, width=80, placeholder_text="0.00002")
         self.entry_gamma.grid(row=5, column=0, columnspan=2, pady=0, padx=10)
@@ -115,6 +113,12 @@ class App(customtkinter.CTk):
                                                     command=self.reset)
         self.reset_button.grid(row=7, column=2, columnspan=1, pady=10, padx=20, sticky="nswe")
         self.right_frame_elements.append(self.reset_button)
+        self.help_button = customtkinter.CTkButton(master=self.frame_right,
+                                                    height=40,
+                                                    text="Справка",
+                                                    command=self.display_help)
+        self.help_button.grid(row=8, column=2, columnspan=1, pady=10, padx=20, sticky="nswe")
+
 
         for elem in self.right_frame_elements:
             elem.configure(state=tkinter.DISABLED)
@@ -147,6 +151,43 @@ class App(customtkinter.CTk):
                 elem.configure(state=tkinter.DISABLED)
             for elem in self.right_frame_elements:
                 elem.configure(state=tkinter.NORMAL)
+
+    def display_help(self):
+        self.textbox = customtkinter.CTkTextbox(master=self.frame_mid, width=540, height=540)
+        self.textbox.grid(row=0, column=0, columnspan=4, pady=10, padx=30, sticky="nswe")
+        self.textbox.insert("0.0", """Я в своем познании настолько преисполнился, что я как будто бы уже
+сто триллионов миллиардов лет проживаю на триллионах и
+триллионах таких же планет, как эта Земля, мне этот мир абсолютно
+понятен, и я здесь ищу только одного - покоя, умиротворения и
+вот этой гармонии, от слияния с бесконечно вечным, от созерцания
+великого фрактального подобия и от вот этого замечательного всеединства
+существа, бесконечно вечного, куда ни посмотри, хоть вглубь - бесконечно
+малое, хоть ввысь - бесконечное большое, понимаешь? А ты мне опять со
+своим вот этим, иди суетись дальше, это твоё распределение, это
+твой путь и твой горизонт познания и ощущения твоей природы, он
+несоизмеримо мелок по сравнению с моим, понимаешь? Я как будто бы уже
+давно глубокий старец, бессмертный, ну или там уже почти бессмертный,
+который на этой планете от её самого зарождения, ещё когда только Солнце
+только-только сформировалось как звезда, и вот это газопылевое облако,
+вот, после взрыва, Солнца, когда оно вспыхнуло, как звезда, начало
+формировать вот эти коацерваты, планеты, понимаешь, я на этой Земле уже
+как будто почти пять миллиардов лет живу и знаю её вдоль и поперёк
+этот весь мир, а ты мне какие-то... мне не важно на твои тачки, на твои
+яхты, на твои квартиры, там, на твоё благо. Я был на этой
+планете бесконечным множеством, и круче Цезаря, и круче Гитлера, и круче
+всех великих, понимаешь, был, а где-то был конченым говном, ещё хуже,
+чем здесь. Я множество этих состояний чувствую. Где-то я был больше
+подобен растению, где-то я больше был подобен птице, там, червю, где-то
+был просто сгусток камня, это всё есть душа, понимаешь? Она имеет грани
+подобия совершенно многообразные, бесконечное множество. Но тебе этого
+не понять, поэтому ты езжай себе , мы в этом мире как бы живем
+разными ощущениями и разными стремлениями, соответственно, разное наше и 
+место, разное и наше распределение. Тебе я желаю все самые крутые тачки
+чтоб были у тебя, и все самые лучше самки, если мало идей, обращайся ко мне, я тебе на каждую твою идею предложу сотню триллионов, как всё делать. Ну а я всё, я иду как глубокий старец,узревший вечное, прикоснувшийся к Божественному, сам стал богоподобен и устремлен в это бесконечное, и который в умиротворении, покое, гармонии, благодати, в этом сокровенном блаженстве пребывает, вовлеченный во всё и во вся, понимаешь, вот и всё, в этом наша разница. Так что я иду любоваться мирозданием, а ты идёшь преисполняться в ГРАНЯХ каких-то, вот и вся разница, понимаешь, ты не зришь это вечное бесконечное, оно тебе не нужно. Ну зато ты, так сказать, более активен, как вот этот дятел долбящий, или муравей, который очень активен в своей стезе, поэтому давай, наши пути здесь, конечно, имеют грани подобия, потому что всё едино, но я-то тебя прекрасно понимаю, а вот ты меня - вряд ли, потому что я как бы тебя в себе содержу, всю твою природу, она составляет одну маленькую там песчиночку, от того что есть во мне, вот и всё, поэтому давай, ступай, езжай, а я пошел наслаждаться прекрасным осенним закатом на берегу теплой южной реки. Всё, ступай, и я пойду.
+""")  # insert at line 0 character 0
+        self.textbox.configure(state="disabled")  # configure textbox to be read-only
+        
+        
 
     def start_plotting(self):
         self.plotter = Plotter(self.frame_mid, *self.parameters_pack)
